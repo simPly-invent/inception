d88229cc68be39920a8c6b07bcd592f6a0ca9a23
simPly-invent <dbenaissi@gmail.com>
2026-08-16 22:37:45 +0200

    wip

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -5,11 +5,12 @@ DB_ROOT_PASSWORD=$(cat /run/secrets/db_root_password)
 
 if [ ! -d /var/lib/mysql/mysql ]; then
     mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null 2>&1
-    mysqld_safe --skip-networking &
+    mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
+
     sleep 5
     mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}'; FLUSH PRIVILEGES;"
     mysql -uroot -p"${DB_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${NAME_DATABASE}; CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}'; GRANT ALL PRIVILEGES ON ${NAME_DATABASE}.* TO '${DB_USER}'@'%'; FLUSH PRIVILEGES;"
     mysqladmin -uroot -p"${DB_ROOT_PASSWORD}" shutdown
 fi
 
-exec mysqld_safe --datadir=/var/lib/mysql
+exec mysqld --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
